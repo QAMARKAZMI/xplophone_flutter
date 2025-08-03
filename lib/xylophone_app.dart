8600c1a7169aa9fa0c75a17ea6d3db7ba64194b0
QAMARKAZMI <syedqammarabbas0@gmail.com>
2025-08-03 14:57:26 +0500

    Refactor sound playback logic and enhance button layout in XylophoneApp

--- lib/xylophone_app.dart
+++ lib/xylophone_app.dart
@@ -9,20 +9,67 @@ class XylophoneApp extends StatefulWidget {
 }
 
 class _XylophoneAppState extends State<XylophoneApp> {
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () {
-              final player = AudioPlayer();
-              player.play(
-                AssetSource('note1.wav'),
-              ); // Ensure you have the audio file in assets
-            },
-            child: Text("Click Me"),
-          ),
+        child: Column(
+          children: [
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
+              onPressed: () {
+                playSound(1); // Ensure you have the audio file in assets
+              },
+              child: Text("Click Me"),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.orange),
+              onPressed: () {
+                playSound(2);
+              },
+              child: Text("Click Me"),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+              onPressed: () {
+                playSound(3);
+              },
+              child: Text("Click Me"),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.green),
+              onPressed: () {
+                playSound(4);
+              },
+              child: Text("Click Me"),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.teal),
+              onPressed: () {
+                playSound(5);
+              },
+              child: Text("Click Me"),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.blue),
+              onPressed: () {
+                playSound(6);
+              },
+              child: Text("Click Me"),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.purple),
+              onPressed: () {
+                playSound(7);
+              },
+              child: Text("Click Me"),
+            ),
+          ],
         ),
       ),
     );
